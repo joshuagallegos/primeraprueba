1d9077c4aca8324b2c2b33c001c413f5196c50df
joshuagallegos <joshualeviantonio@gmail.com>
2019-03-10 17:27:06 -0600

    Primerapruebaparasubirproyectoengithub

---
+++ MiFiltroMVC/MiFiltroMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiFiltroMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        // GET: Home
+        public string Index()
+        {
+            return "Este es mi controlador Home";
+        }
+
+        [Authorize (Roles = "admin")]
+        [OutputCache (Duration = 10)]
+
+        public string HoraActual(){
+            return DateTime.Now.ToString("T");
+        }
+    }
+}
